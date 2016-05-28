323ca27830b5f8c7f35b4073f689b6c2a1f7dabb
Eduardo Dobay <edudobay@gmail.com>
2016-05-28 16:50:57 -0300

    Integrate webpack with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var slim = require("gulp-slim");
 var sass = require("gulp-sass");
+var webpack = require("webpack-stream");
 
 gulp.task('slim', function() {
     return gulp.src("html/**/*.slim")
@@ -14,8 +15,15 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('public/css'));
 });
 
+gulp.task('webpack', function() {
+    return gulp.src('js/main.js')
+      .pipe(webpack(require('./webpack.config.js')))
+      .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('watch', function() {
     gulp.watch('html/**/*.slim', ['slim']);
     gulp.watch('css/**/*.sass', ['sass']);
+    gulp.watch('js/**/*.js', ['webpack']);
 });
 
