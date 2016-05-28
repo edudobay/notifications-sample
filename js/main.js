25e7dfb7005dff2fc27c951d308786dc17c69ba2
Eduardo Dobay <edudobay@gmail.com>
2016-05-28 16:38:25 -0300

    Add sample Toastr usage, loading CSS via webpack

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const $ = require('jquery');
+const toastr = require('toastr');
+require('!style!css!toastr/package/toastr.css');
 
 function main() {
-    $('#content').append('<p>hello world</p>');
+    const p = $('<p>hello world</p>');
+    const counter = { times: 0 };
+    p.appendTo('#content')
+        .on('click', () =>
+            toastr.info(`This paragraph has been clicked ${++counter.times} times`));
 }
 
 $(main);
